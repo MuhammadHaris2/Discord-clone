75d30f63e62eeffaaba2d95b299964153047ecfc
MuhammadHaris2 <mhari8339@gmail.com>
2020-10-12 15:31:05 +0500

    Updates

---
+++ precache-manifest.ec0b9a472dd78b985573394e91f512d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22b2e4a8e801dd66db0c77d417e06e7",
+    "url": "/Discord-clone/index.html"
+  },
+  {
+    "revision": "85e9b98d91f6ce9846ed",
+    "url": "/Discord-clone/static/css/main.d25c96db.chunk.css"
+  },
+  {
+    "revision": "9b5205943b29e2c106a1",
+    "url": "/Discord-clone/static/js/2.304e20fb.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Discord-clone/static/js/2.304e20fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e9b98d91f6ce9846ed",
+    "url": "/Discord-clone/static/js/main.e7dbc9fd.chunk.js"
+  },
+  {
+    "revision": "e75c6104b4993bf9cdc6",
+    "url": "/Discord-clone/static/js/runtime-main.7747400e.js"
+  }
+]);
